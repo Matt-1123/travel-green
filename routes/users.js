7905ea0c2ee27347524fc1d979dbb829af416417
Matt Russo <matthew.russo91@gmail.com>
2021-11-29 19:12:14 -0500

    Revert "user registration: create and respond w/ JSON web token"
    This reverts commit 85580b963e4ddbc97051b255438c718ff64e7a15.

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 // import User model
@@ -53,26 +51,7 @@ router.post(
       // Save user to database.
       await user.save();
 
-      // payload is the object we'll send in the token
-      // We only need to send in the user ID because that's all that is needed to access user info.
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-
-      // Generate a token by signing it. Respond with the token in the response.
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        {
-          expiresIn: 360000,
-        },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
+      res.send("User saved");
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
