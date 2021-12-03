85f0f8eb5b2149da008d9b53a5a6a8154f6e9c2c
Matt Russo <matthew.russo91@gmail.com>
2021-12-02 21:40:47 -0500

    add action route

--- routes/actions.js
+++ routes/actions.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
 const Action = require("../models/Action");
 
 // @route     GET api/actions
@@ -19,9 +20,58 @@ router.get("/", auth, async (req, res) => {
 // @route     POST api/actions
 // @desc      Add new action
 // @access    Private
-router.post("/", (req, res) => {
-  res.send("Post an action");
-});
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("title", "Title is required").not().isEmpty(),
+      check("impact", "A number for kg CO2 prevented is required")
+        .not()
+        .isEmpty(),
+      check("travelTypeUsed", "Travel type used is required").not().isEmpty(),
+      check("travelTypeAvoided", "Travel type avoided is required")
+        .not()
+        .isEmpty(),
+      check("actionType", "Action type is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    // Validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      actionType,
+      date,
+      title,
+      impact,
+      travelTypeUsed,
+      travelTypeAvoided,
+    } = req.body;
+
+    try {
+      const newAction = new Action({
+        actionType,
+        date,
+        title,
+        impact,
+        travelTypeUsed,
+        travelTypeAvoided,
+        user: req.user.id,
+      });
+
+      const action = await newAction.save();
+
+      res.json(action);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // @route     PUT api/actions/:id
 // @desc      Update action
