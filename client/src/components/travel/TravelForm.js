cc3d4b57d2307f4b1a4572b0471120109847743a
Matt Russo <matthew.russo91@gmail.com>
2021-12-06 21:32:28 -0500

    add Google Places Autocomplete to travel form

--- client/src/components/travel/TravelForm.js
+++ client/src/components/travel/TravelForm.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+import GoogleMapReact from "google-map-react";
 
 const TravelForm = () => {
   const [travel, setTravel] = useState({
@@ -25,8 +30,20 @@ const TravelForm = () => {
     avoidedDestination,
   } = travel;
 
+  console.log(process.env);
+
   const onChange = (e) =>
     setTravel({ ...travel, [e.target.name]: e.target.value });
+
+  const handlePlacesChange = (usedOrigin) =>
+    setTravel({ ...travel, usedOrigin });
+
+  const handleSelect = (usedOrigin) => {
+    geocodeByAddress(usedOrigin)
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => console.log("Success", latLng))
+      .catch((error) => console.error("Error", error));
+  };
 
   return (
     <form className="form-container">
@@ -78,7 +95,60 @@ const TravelForm = () => {
         </div>
         <div className="form-group">
           <label htmlFor="usedOrigin">Origin</label>
-          <input type="text" id="usedOrigin" placeholder="Enter a location" />
+          <input
+            type="text"
+            id="usedOrigin"
+            placeholder="Enter a location"
+            onChange={onChange}
+          />
+          <PlacesAutocomplete
+            name="usedOrigin"
+            value={usedOrigin}
+            onChange={handlePlacesChange}
+            onSelect={handleSelect}
+          >
+            {({
+              getInputProps,
+              suggestions,
+              getSuggestionItemProps,
+              loading,
+            }) => (
+              // <div>
+              //   <input type="text" placeholder="Enter a location" />
+              // </div>
+
+              <div>
+                <input
+                  {...getInputProps({
+                    placeholder: "Search Places ...",
+                    className: "location-search-input",
+                  })}
+                />
+                <div className="autocomplete-dropdown-container">
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map((suggestion) => {
+                    const className = suggestion.active
+                      ? "suggestion-item--active"
+                      : "suggestion-item";
+                    // inline style for demonstration purpose
+                    const style = suggestion.active
+                      ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                      : { backgroundColor: "#ffffff", cursor: "pointer" };
+                    return (
+                      <div
+                        {...getSuggestionItemProps(suggestion, {
+                          className,
+                          style,
+                        })}
+                      >
+                        <span>{suggestion.description}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
         </div>
         <div className="form-group">
           <label htmlFor="usedDestination">Destination</label>
