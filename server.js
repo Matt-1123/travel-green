4f6825354042fcd7a22e3874c7feda2db9059053
Matt Russo <matthew.russo91@gmail.com>
2022-02-15 15:06:14 -0500

    prepare for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 require("dotenv").config();
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -12,16 +13,21 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the Travel Green API" })
-);
-
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/actions", require("./routes/actions"));
 app.use("/api/carbon-interface", require("./routes/carbon-interface"));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
