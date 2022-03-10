f8f2c4da6e717dc3f416480af45229d136ad7b3a
Matt Russo <matthew.russo91@gmail.com>
2022-03-09 19:13:23 -0500

    'actions options' sidebar style changes

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -7,29 +7,32 @@ const Home = () => {
   const authContext = useContext(AuthContext);
 
   return (
-    <div className="grid-2-5-2" style={{ alignItems: "start" }}>
-      <div className="card bg-dark">
+    <div className="grid-2-5-2 my-2" style={{ alignItems: "start" }}>
+      <div className="container">
         <h2 className="text-left">Actions</h2>
-        <p>Choose an action type</p>
+        <hr />
+        <p className="mt-1" style={{ lineHeight: "1.125" }}>
+          Choose an action type:
+        </p>
         <ul className="list">
           <li>
             <Link to="/add-travel">Travel</Link>
           </li>
         </ul>
       </div>
+
       <div>
         <Feed />
       </div>
-      <div>
-        <div className="container">
-          <h2 className="text-left">Learn More</h2>
-          <hr />
-          <ul className="list">
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </div>
+
+      <div className="container">
+        <h2 className="text-left">Learn More</h2>
+        <hr />
+        <ul className="list">
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
