57f14f238f5eff445d7371cd6e96e6f62c340ec2
Matt Russo <matthew.russo91@gmail.com>
2022-03-25 08:25:06 -0400

    About page minor updates

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="container-narrow">
+      <h1>About PEH</h1>
       <hr />
       <p className="my-1">
         Project Earth Health is a platform where you can calculate and log
@@ -14,14 +14,15 @@ const About = () => {
       <h2>Future Updates/Additions</h2>
       <hr />
       <ul>
-        <li class="ml-1 my" style={{ listStyleType: "circle" }}>
+        <li className="ml-1 my" style={{ listStyleType: "circle" }}>
           Transit will be added soon as a Travel Action option.
         </li>
-        <li class="ml-1 my" style={{ listStyleType: "circle" }}>
+        <li className="ml-1 my" style={{ listStyleType: "circle" }}>
           New action types will be added after Travel Action is complete. The
-          next Action Type will be a Diet Action, in which users will be able to
-          calculate the impacts of changes in their diet, such as decreasing
-          their weekly beef consumption.
+          next Action Type will be a Diet Action, in which you will be able to
+          calculate the impacts of choosing more environmentally-friendly food
+          options, such as replacing beef with chicken for a month, or going
+          meat and cheese free for a week.
         </li>
       </ul>
     </div>
